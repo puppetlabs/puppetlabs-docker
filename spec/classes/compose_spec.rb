02810be931b393f29ef9571cf767470a9183feb5
Gene Liverman <gene.liverman+06301990@puppet.com>
2018-02-21 11:00:05 -0800

    Updated to the latest version of docker-compose
    The previous version does not support v3 of docker-compose.yml... this
    one does.

--- spec/classes/compose_spec.rb
+++ spec/classes/compose_spec.rb
@@ -18,28 +18,28 @@ describe 'docker::compose', :type => :class do
 
   context 'with defaults for all parameters' do
     it { should compile.with_all_deps }
-    it { should contain_exec('Install Docker Compose 1.9.0').with(
+    it { should contain_exec('Install Docker Compose 1.19.0').with(
       'path'    => '/usr/bin/',
       'cwd'     => '/tmp',
-      'command' => 'curl -s -S -L  https://github.com/docker/compose/releases/download/1.9.0/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose-1.9.0',
-      'creates' => '/usr/local/bin/docker-compose-1.9.0',
+      'command' => 'curl -s -S -L  https://github.com/docker/compose/releases/download/1.19.0/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose-1.19.0',
+      'creates' => '/usr/local/bin/docker-compose-1.19.0',
       'require' => 'Package[curl]'
     )}
-    it { should contain_file('/usr/local/bin/docker-compose-1.9.0').with(
+    it { should contain_file('/usr/local/bin/docker-compose-1.19.0').with(
       'owner'   => 'root',
       'mode'    => '0755',
-      'require' => 'Exec[Install Docker Compose 1.9.0]'
+      'require' => 'Exec[Install Docker Compose 1.19.0]'
     )}
     it { should contain_file('/usr/local/bin/docker-compose').with(
       'ensure'   => 'link',
-      'target'   => '/usr/local/bin/docker-compose-1.9.0',
-      'require'  => 'File[/usr/local/bin/docker-compose-1.9.0]'
+      'target'   => '/usr/local/bin/docker-compose-1.19.0',
+      'require'  => 'File[/usr/local/bin/docker-compose-1.19.0]'
     )}
   end
 
   context 'with ensure => absent' do
     let (:params) { { :ensure => 'absent' } }
-    it { should contain_file('/usr/local/bin/docker-compose-1.9.0').with_ensure('absent') }
+    it { should contain_file('/usr/local/bin/docker-compose-1.19.0').with_ensure('absent') }
     it { should contain_file('/usr/local/bin/docker-compose').with_ensure('absent') }
   end
 
