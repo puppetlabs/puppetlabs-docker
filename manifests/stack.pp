b4588490d579f3cfb2b74c8f209fb9a238f05fad
Mark Wilson <mark.wilson@puppetlabs.com>
2018-07-05 22:06:24 +0100

    CLOUD-1967-Fixing-issue##243

--- manifests/stack.pp
+++ manifests/stack.pp
@@ -36,13 +36,13 @@
 
 define docker::stack(
 
-  Optional[Pattern[/^present$|^absent$/]] $ensure             = 'present',
-  Optional[String] $stack_name                                = undef,
-  Optional[String] $bundle_file                               = undef,
-  Optional[String] $compose_file                              = undef,
-  Optional[String] $prune                                     = undef,
-  Optional[String] $with_registry_auth                        = undef,
-
+  Optional[Pattern[/^present$|^absent$/]] $ensure                = 'present',
+  Optional[String] $stack_name                                   = undef,
+  Optional[String] $bundle_file                                  = undef,
+  Optional[String] $compose_file                                 = undef,
+  Optional[String] $prune                                        = undef,
+  Optional[String] $with_registry_auth                           = undef,
+  Optional[Pattern[/^always$|^changed$|^never$/]] $resolve_image = undef,
   ){
 
   include docker::params
@@ -56,6 +56,7 @@ define docker::stack(
       compose_file => $compose_file,
       prune => $prune,
       with_registry_auth => $with_registry_auth,
+      resolve_image => $resolve_image,
       })
 
       $exec_stack = "${docker_command} deploy ${docker_stack_flags} ${stack_name}"
