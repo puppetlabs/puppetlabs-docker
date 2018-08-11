d91265907ba75810964fe423c167a9d458bfffb2
Hugh Esco <hesco@yourmessagedelivered.com>
2018-08-11 02:34:39 -0400

    clean up syntax issues for docker::firewall::swarm_init

--- manifests/firewall/swarm_init.pp
+++ manifests/firewall/swarm_init.pp
@@ -11,7 +11,7 @@
 class docker::firewall::swarm_init {
 
   # -A FORWARD -o docker_gwbridge -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-  firewall { "00131: ACCEPT RELATED, ESTABLISHED bound for docker_gwbridge":
+  firewall { '00131: ACCEPT RELATED, ESTABLISHED bound for docker_gwbridge':
     chain    => 'FORWARD',
     outiface => 'docker_gwbridge',
     ctstate  => [ 'RELATED', 'ESTABLISHED'],
@@ -19,14 +19,14 @@ class docker::firewall::swarm_init {
   }
 
   # -A FORWARD -o docker_gwbridge -j DOCKER
-  firewall { "00132: outbound to docker_gwbridge jumps to DOCKER chain":
+  firewall { '00132: outbound to docker_gwbridge jumps to DOCKER chain':
     chain    => 'FORWARD',
     outiface => 'docker_gwbridge',
     jump     => 'DOCKER',
   }
 
   # -A FORWARD -i docker_gwbridge ! -o docker_gwbridge -j ACCEPT
-  firewall { "00133: ACCEPT traffic inbound on docker_gwbridge":
+  firewall { '00133: ACCEPT traffic inbound on docker_gwbridge':
     chain    => 'FORWARD',
     iniface  => 'docker_gwbridge',
     outiface => '! docker_gwbridge',
@@ -34,7 +34,7 @@ class docker::firewall::swarm_init {
   }
 
   # -A FORWARD -i docker_gwbridge -o docker_gwbridge -j DROP
-  firewall { "00134: DROP traffic inbound and outbound on docker_gwbridge":
+  firewall { '00134: DROP traffic inbound and outbound on docker_gwbridge':
     chain    => 'FORWARD',
     iniface  => 'docker_gwbridge',
     outiface => 'docker_gwbridge',
@@ -42,22 +42,22 @@ class docker::firewall::swarm_init {
   }
 
   # -A DOCKER-ISOLATION-STAGE-1 -i docker_gwbridge ! -o docker_gwbridge -j DOCKER-ISOLATION-STAGE-2
-  firewall { "00135: pass packets through DOCKER-ISOLATION from STAGE-1 to STAGE-2":
+  firewall { '00135: pass packets through DOCKER-ISOLATION from STAGE-1 to STAGE-2':
     chain    => 'DOCKER-ISOLATION-STAGE-1',
     iniface  => 'docker_gwbridge',
     outiface => '! docker_gwbridge',
-    jump     => 'DOCKER-ISOLATION-STAGE-2'.
+    jump     => 'DOCKER-ISOLATION-STAGE-2',
   }
 
   # -A DOCKER-ISOLATION-STAGE-2 -o docker_gwbridge -j DROP
-  firewall { "00136: DROP STAGE-2 packets outbound on docker_gwbridge":
+  firewall { '00136: DROP STAGE-2 packets outbound on docker_gwbridge':
     chain    => 'DOCKER-ISOLATION-STAGE-2',
     outiface => 'docker_gwbridge',
     action   => drop,
   }
 
   # -A POSTROUTING -s 172.18.0.0/16 ! -o docker_gwbridge -j MASQUERADE
-  firewall { "00137: MASQUEARDE docker_gwbridge sourced packets":
+  firewall { '00137: MASQUEARDE docker_gwbridge sourced packets':
     chain    => 'POSTROUTING',
     source   => '172.18.0.0/16',
     outiface => '! docker_gwbridge',
@@ -65,7 +65,7 @@ class docker::firewall::swarm_init {
   }
 
   # -A DOCKER -i docker_gwbridge -j RETURN
-  firewall { "60138: RETURN packets from docker_gwbridge":
+  firewall { '60138: RETURN packets from docker_gwbridge':
     chain    => 'DOCKER',
     iniface  => 'docker_gwbridge',
     jump     => 'RETURN',
