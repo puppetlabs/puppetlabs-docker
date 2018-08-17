36c7e96e7e69fd833b2e32bca4f70ccdce86b909
Hugh Esco <hesco@yourmessagedelivered.com>
2018-08-17 09:30:01 -0400

    define service_array for docker_swarm fact

--- lib/facter/docker_swarm.rb
+++ lib/facter/docker_swarm.rb
@@ -5,7 +5,7 @@ Facter.add(:docker_swarm) do
     services_hash = {}
     services_hash['service_ids'] = []
     docker_binary = Facter::Core::Execution.exec('/usr/bin/which docker')
-    swarm_status = Facter::Core::Execution.exec("/usr/bin/docker info -f '{{json .Swarm.ControlAvailable }}'")
+    swarm_status = Facter::Core::Execution.exec("#{docker_binary} info -f '{{json .Swarm.ControlAvailable }}'")
     if docker_binary !~ %r{docker}
       services_hash['docker_host'] = 'not a docker host'
     elsif swarm_status == false
@@ -14,13 +14,14 @@ Facter.add(:docker_swarm) do
       docker_hostname = Facter::Core::Execution.exec('hostname -f')
       services_hash['docker_host'] = docker_hostname
       service_count = 0
+      service_array = Facter::Core::Execution.exec("#{docker_binary} service ls | /usr/bin/awk '{ print $2 }'")
       if service_array
         service_array.each do |service_id|
           service_count += 1
-          service_inspect_json = Facter::Core::Execution.exec("/usr/bin/docker inspect #{service_id}")
+          service_inspect_json = Facter::Core::Execution.exec("#{docker_binary} inspect #{service_id}")
           service_meta_data = JSON.parse(service_inspect_json)
-          hostname = Facter::Core::Execution.exec("/usr/bin/docker inspect -f \"{{ .Name }}\" #{service_id}").sub(%r{^/}, '')
-          ip = Facter::Core::Execution.exec("/usr/bin/docker inspect -f \"{{ .NetworkSettings.IPAddress }}\" #{service_id}")
+          hostname = Facter::Core::Execution.exec("#{docker_binary} inspect -f \"{{ .Name }}\" #{service_id}").sub(%r{^/}, '')
+          ip = Facter::Core::Execution.exec("#{docker_binary} inspect -f \"{{ .NetworkSettings.IPAddress }}\" #{service_id}")
           if hostname
             services_hash[hostname] = service_meta_data[0]
           elsif ip
