9f57e83f4190defdef778d859f98b7b24af481eb
Hugh Esco <hesco@yourmessagedelivered.com>
2018-08-17 09:14:32 -0400

    first pass, rough out docker_swarm fact

---
+++ lib/facter/docker_swarm.rb
@@ -0,0 +1,36 @@
+require 'json'
+
+Facter.add(:docker_swarm) do
+  setcode do
+    services_hash = {}
+    services_hash['service_ids'] = []
+    docker_binary = Facter::Core::Execution.exec('/usr/bin/which docker')
+    swarm_status = Facter::Core::Execution.exec("/usr/bin/docker info -f '{{json .Swarm.ControlAvailable }}'")
+    if docker_binary !~ %r{docker}
+      services_hash['docker_host'] = 'not a docker host'
+    elsif swarm_status == false
+      services_hash['docker_host'] = 'docker host not running swarm'
+    else
+      docker_hostname = Facter::Core::Execution.exec('hostname -f')
+      swarm['docker_host'] = docker_hostname
+      service_count = 0
+      if service_array
+        service_array.each do |service_id|
+          service_count += 1
+          service_inspect_json = Facter::Core::Execution.exec("/usr/bin/docker inspect #{service_id}")
+          service_meta_data = JSON.parse(service_inspect_json)
+          hostname = Facter::Core::Execution.exec("/usr/bin/docker inspect -f \"{{ .Name }}\" #{service_id}").sub(%r{^/}, '')
+          ip = Facter::Core::Execution.exec("/usr/bin/docker inspect -f \"{{ .NetworkSettings.IPAddress }}\" #{service_id}")
+          if hostname
+            services_hash[hostname] = service_meta_data[0]
+          elsif ip
+            services_hash[ip] = service_meta_data
+          end
+          services_hash['service_ids'].push(service_id)
+        end
+      end
+      services_hash['service_count'] = service_count
+    end
+    services_hash
+  end
+end
