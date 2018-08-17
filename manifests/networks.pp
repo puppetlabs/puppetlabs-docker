6a517087967ffc615581cf3783236ab0b9beb54c
Hugh Esco <hesco@yourmessagedelivered.com>
2018-08-17 10:40:59 -0400

    want $facts, not undefined $facter to access Facter::facts

--- manifests/networks.pp
+++ manifests/networks.pp
@@ -1,7 +1,7 @@
 # docker::networks
 class docker::networks($networks) {
-  if ( $facter[docker_swarm][swarm][ControlAvailable] == true )
-       or ( $facter[docker_swarm][swarm] == 'docker host not running swarm' ) {
+  if ( $facts[docker_swarm][swarm][ControlAvailable] == true )
+       or ( $facts[docker_swarm][swarm] == 'docker host not running swarm' ) {
     create_resources(docker_network, $networks)
   }
 }
