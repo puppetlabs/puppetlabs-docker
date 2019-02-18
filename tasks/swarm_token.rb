de3f0005e9e7a2b9ead229dea56339f023edcf66
Kevin Häfeli <khaefeli@users.noreply.github.com>
2019-02-18 10:46:56 +0100

    Fix frozen string error (#434)

--- tasks/swarm_token.rb
+++ tasks/swarm_token.rb
@@ -7,7 +7,7 @@ require 'puppet'
 
 def swarm_token(node_role)
   cmd_string = 'docker swarm join-token -q'
-  cmd_string << " #{node_role}" unless node_role.nil?
+  cmd_string += " #{node_role}" unless node_role.nil?
 
   stdout, stderr, status = Open3.capture3(cmd_string)
   raise Puppet::Error, "stderr: '#{stderr}'" if status != 0
