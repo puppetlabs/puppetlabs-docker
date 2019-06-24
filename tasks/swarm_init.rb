3772c5b5bb1640184e8c4f8ae91c34683ae6ccc6
Kevin Häfeli <khaefeli@users.noreply.github.com>
2019-06-24 10:23:08 +0200

    Fix frozen string errors for tasks (#499)

--- tasks/swarm_init.rb
+++ tasks/swarm_init.rb
@@ -7,15 +7,15 @@ require 'puppet'
 
 def swarm_init(advertise_addr, autolock, cert_expiry, dispatcher_heartbeat, external_ca, force_new_cluster, listen_addr, max_snapshots, snapshot_interval)
   cmd_string = 'docker swarm init'
-  cmd_string << " --advertise-addr=#{advertise_addr}" unless advertise_addr.nil?
-  cmd_string << ' --autolock' unless autolock.nil?
-  cmd_string << ' --cert-expiry' unless cert_expiry.nil?
-  cmd_string << " --dispatcher-heartbeat=#{dispatcher_heartbeat}" unless dispatcher_heartbeat.nil?
-  cmd_string << " --external-ca=#{external_ca}" unless external_ca.nil?
-  cmd_string << ' --force-new-cluster' unless force_new_cluster.nil?
-  cmd_string << " --listen-addr=#{listen_addr}" unless listen_addr.nil?
-  cmd_string << " --max-snapshots=#{max_snapshots}" unless max_snapshots.nil?
-  cmd_string << " --snapshot-interval=#{snapshot_interval}" unless snapshot_interval.nil?
+  cmd_string += " --advertise-addr=#{advertise_addr}" unless advertise_addr.nil?
+  cmd_string += ' --autolock' unless autolock.nil?
+  cmd_string += ' --cert-expiry' unless cert_expiry.nil?
+  cmd_string += " --dispatcher-heartbeat=#{dispatcher_heartbeat}" unless dispatcher_heartbeat.nil?
+  cmd_string += " --external-ca=#{external_ca}" unless external_ca.nil?
+  cmd_string += ' --force-new-cluster' unless force_new_cluster.nil?
+  cmd_string += " --listen-addr=#{listen_addr}" unless listen_addr.nil?
+  cmd_string += " --max-snapshots=#{max_snapshots}" unless max_snapshots.nil?
+  cmd_string += " --snapshot-interval=#{snapshot_interval}" unless snapshot_interval.nil?
 
   stdout, stderr, status = Open3.capture3(cmd_string)
   raise Puppet::Error, "stderr: '#{stderr}'" if status != 0
