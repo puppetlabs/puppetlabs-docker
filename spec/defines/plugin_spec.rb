52cfed1b5b8e8c395c7b3ef6281e8ab14c1d28e0
Kosh <koshatul@users.noreply.github.com>
2019-08-20 14:37:47 +1000

    Added RSpec tests

--- spec/defines/plugin_spec.rb
+++ spec/defines/plugin_spec.rb
@@ -15,20 +15,27 @@ describe 'docker::plugin', type: :define do
 
   context 'with defaults for all parameters' do
     it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_exec('plugin install foo/plugin:latest').with_command(%r{docker plugin install}) }
+    it { is_expected.to contain_exec('plugin install foo/plugin:latest').with_command(%r{docker plugin install}).with_unless(%r{docker ls --format='{{.PluginReference}}' | grep -w foo/plugin:latest}) }
   end
 
   context 'with enabled => false' do
     let(:params) { { 'enabled' => false } }
 
     it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_exec('disable foo/plugin:latest').with_command(%r{docker plugin disable}) }
+    it { is_expected.to contain_exec('disable foo/plugin:latest').with_command(%r{docker plugin disable}).with_unless(%r{docker ls --format='{{.PluginReference}}' | grep -w foo/plugin:latest}) }
   end
 
   context 'with ensure => absent' do
     let(:params) { { 'ensure' => 'absent' } }
 
     it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_exec('plugin remove foo/plugin:latest').with_command(%r{docker plugin rm}) }
+    it { is_expected.to contain_exec('plugin remove foo/plugin:latest').with_command(%r{docker plugin rm}).with_onlyif(%r{docker ls --format='{{.PluginReference}}' | grep -w foo/plugin:latest}) }
   end
+
+  context 'with alias => foo-plugin' do
+    let(:params) { { 'plugin_alias' => 'foo-plugin' } }
+
+    it { is_expected.to contain_exec('plugin install foo/plugin:latest').with_command(%r{docker plugin install}).with_unless(%r{docker ls --format='{{.PluginReference}}' | grep -w foo/plugin:latest}) }
+  end
+
 end
