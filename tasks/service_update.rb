c02e04275f672e70578339c305c063ab5bc473f6
Kevin Häfeli <kevin@helio.exchange>
2019-11-13 21:26:55 +0100

    :bug: Fix wrong function names

--- tasks/service_update.rb
+++ tasks/service_update.rb
@@ -5,7 +5,7 @@ require 'json'
 require 'open3'
 require 'puppet'
 
-def swarm_update(image, service)
+def service_update(image, service)
   cmd_string = 'docker service update'
   cmd_string += " --image #{image}" unless image.nil?
   cmd_string += " #{service}" unless service.nil?
@@ -20,7 +20,7 @@ image = params['image']
 service = params['service']
 
 begin
-  result = swarm_update(image, service)
+  result = service_update(image, service)
   puts result
   exit 0
 rescue Puppet::Error => e
