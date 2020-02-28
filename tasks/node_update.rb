4976802e0b6c7d80fa95bcba582edb287698a4b2
Flavian <flavian.sierk@gmail.com>
2020-02-28 11:39:39 +0100

    fixes and extens docker node update to add/remove labels0

--- tasks/node_update.rb
+++ tasks/node_update.rb
@@ -5,11 +5,24 @@ require 'json'
 require 'open3'
 require 'puppet'
 
-def node_update(availability, role, nodeid)
+def node_update(availability, role, label_add, label_rm, node)
   cmd_string = 'docker node update'
   cmd_string += " --availability #{availability}" unless availability.nil?
   cmd_string += " --role #{role}" unless role.nil?
-  cmd_string += " #{nodeid}" unless nodeid.nil?
+
+  if label_add.is_a? Array
+    label_add.each do |param|
+      cmd_string += " --label-add #{param}"
+    end
+  end
+
+  if label_rm.is_a? Array
+    label_rm.each do |param|
+      cmd_string += " --label-rm #{param}"
+    end
+  end
+
+  cmd_string += " #{node}" unless node.nil?
 
   stdout, stderr, status = Open3.capture3(cmd_string)
   raise Puppet::Error, "stderr: '#{stderr}'" if status != 0
@@ -19,10 +32,12 @@ end
 params = JSON.parse(STDIN.read)
 availability = params['availability']
 role = params['role']
-nodeid = params['nodeid']
+label_add = params['label_add']
+label_rm = params['label_rm']
+node = params['node']
 
 begin
-  result = node_update(availability, role, nodeid)
+  result = node_update(availability, role, label_add, label_rm, node)
   puts result
   exit 0
 rescue Puppet::Error => e
---
+++ tasks/swarm_update.rb
@@ -0,0 +1,20 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+# frozen_string_literal: true
+
+require 'json'
+require 'open3'
+require 'puppet'
+
+params = JSON.parse(STDIN.read)
+image = params['image']
+service = params['service']
+
+begin
+  puts "Deprecated: use docker::service_update instead"
+  result = service_update(image, service)
+  puts result
+  exit 0
+rescue Puppet::Error => e
+  puts(status: 'failure', error: e.message)
+  exit 1
+end
