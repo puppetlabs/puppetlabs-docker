b72a4094d5374d4f9eebfb8145c17ec7384c41d5
Flavian <flavian.sierk@gmail.com>
2020-03-05 09:36:08 +0100

    add/remove service constraints

--- tasks/service_update.rb
+++ tasks/service_update.rb
@@ -5,9 +5,22 @@ require 'json'
 require 'open3'
 require 'puppet'
 
-def service_update(image, service)
+def service_update(image, service, constraint_add, constraint_rm)
   cmd_string = 'docker service update'
   cmd_string += " --image #{image}" unless image.nil?
+
+  if constraint_add.is_a? Array
+    constraint_add.each do |param|
+      cmd_string += " --constraint-add #{param}"
+    end
+  end
+
+  if constraint_rm.is_a? Array
+    constraint_rm.each do |param|
+      cmd_string += " --constraint-rm #{param}"
+    end
+  end
+
   cmd_string += " #{service}" unless service.nil?
 
   stdout, stderr, status = Open3.capture3(cmd_string)
@@ -18,9 +31,11 @@ end
 params = JSON.parse(STDIN.read)
 image = params['image']
 service = params['service']
+constraint_add = params['constraint_add']
+constraint_rm = params['constraint_rm']
 
 begin
-  result = service_update(image, service)
+  result = service_update(image, service, constraint_add, constraint_rm)
   puts result
   exit 0
 rescue Puppet::Error => e
