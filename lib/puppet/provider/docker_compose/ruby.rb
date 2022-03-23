5681f0f85fe1d5a27ed2b69f09110619d6827629
canihavethisone <spam54321@hotmail.com>
2022-03-23 22:26:55 +1100

    Update ruby.rb

--- lib/puppet/provider/docker_compose/ruby.rb
+++ lib/puppet/provider/docker_compose/ruby.rb
@@ -31,26 +31,25 @@ Puppet::Type.type(:docker_compose).provide(:ruby) do
                           "label=com.docker.compose.project=#{name}",
                         ]).split("\n")
     compose_containers.push(*containers)
-    compose_containers.uniq!
 
     compose_services = compose_output['services']
 
-    if compose_services.count != compose_containers.count
+    if compose_services.count != compose_containers.uniq.count
       return false
     end
 
     counts = Hash[*compose_services.each.map { |key, array|
                     image = (array['image']) ? array['image'] : get_image(key, compose_services)
                     Puppet.info("Checking for compose service #{key} #{image}")
-                    ["#{key}-#{image}", compose_containers.count("#{key}-#{image}")]
+                    ["#{key}", compose_containers.count("#{key}-#{image}")]
                   }.flatten]
 
     # No containers found for the project
     if counts.empty? ||
-       # Containers described in the compose file are not running
-       counts.any? { |_k, v| v.zero? } ||
-       # The scaling factors in the resource do not match the number of running containers
-       resource[:scale] && counts.merge(resource[:scale]) != counts
+      # Containers described in the compose file are not running
+      counts.any? { |_k, v| v.zero? } ||
+      # The scaling factors in the resource do not match the number of running containers
+      resource[:scale] && counts.merge(resource[:scale]) != counts
       false
     else
       true
