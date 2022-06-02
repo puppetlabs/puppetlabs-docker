c01ee3714b5a61e73776c79de1f882eb5cffbd25
Steve Traylen <steve.traylen@cern.ch>
2022-06-02 21:27:58 +0200

    New create_user parameter on main class
    Currently when specifying `docker_users` every user is
    both created and added to the docker group.
    
    If `create_user` is false then the users are presumed to exist
    via another method outside this module and will only be added to
    the docker group.
    
    Fixes #840

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,9 +1,12 @@
-# @summary
+# @summary Configuration for docker
+# @api private
 #
 class docker::config {
   if $facts['os']['family'] != 'windows' {
     $docker::docker_users.each |$user| {
-      docker::system_user { $user: }
+      docker::system_user { $user:
+        create_user => $docker::create_user,
+      }
     }
   } else {
     $docker::docker_users.each |$user| {
