8ea8dd0001abc7c2d4a4d77bef91df25f0a5409f
Steffen Zieger <me@saz.sh>
2023-02-15 10:53:16 +0100

    Fix aarch64 architecture in apt repo
    dpkg expects `arm64`, while the kernel reports `aarch64`

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -22,10 +22,16 @@ class docker::repos (
       $package_key   = $docker::package_key
       $package_repos = $docker::package_repos
 
+      if $architecture == 'aarch64' {
+        $_architecture = 'arm64'
+      } else {
+        $_architecture = $architecture
+      }
+
       if ($docker::use_upstream_package_source) {
         apt::source { 'docker':
           location     => $location,
-          architecture => $architecture,
+          architecture => $_architecture,
           release      => $release,
           repos        => $package_repos,
           key          => {
