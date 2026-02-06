ac431430d55b38b145e119600921f38c4110af5c
Shubham Shinde <shubham.shinde@perforce.com>
2026-02-06 10:46:24 +0530

    (MODULES-11645) Fix rubocop offenses

--- spec/unit/lib/puppet/provider/docker_compose/ruby_spec.rb
+++ spec/unit/lib/puppet/provider/docker_compose/ruby_spec.rb
@@ -7,14 +7,14 @@ describe Puppet::Type.type(:docker_compose).provider(:ruby) do
     Puppet::Type.type(:docker_compose).new(
       name: 'test_project',
       compose_files: ['/tmp/docker-compose.yml'],
-      provider: :ruby
+      provider: :ruby,
     )
   end
 
   let(:provider) { resource.provider }
 
   describe '#exists?' do
-    before do
+    before(:each) do
       allow(Puppet::Util::Platform).to receive(:windows?).and_return(false)
       allow(provider).to receive(:execute).and_return(YAML.safe_load("services:\n  web:\n    image: mysql\n    restart: always\n"))
       allow(provider).to receive(:docker).and_return("'db-mysql'\n")
@@ -23,11 +23,11 @@ describe Puppet::Type.type(:docker_compose).provider(:ruby) do
     it 'ignores services with restart: no' do
       # Mock the compose_output to have services with restart: 'no'
       allow(Puppet::Util::Yaml).to receive(:safe_load).and_return({
-        'services' => {
-          'web' => { 'image' => 'nginx', 'restart' => 'no' },
-          'db' => { 'image' => 'mysql', 'restart' => 'always' }
-        }
-      })
+                                                                    'services' => {
+                                                                      'web' => { 'image' => 'nginx', 'restart' => 'no' },
+                                                                      'db' => { 'image' => 'mysql', 'restart' => 'always' }
+                                                                    }
+                                                                  })
 
       # Only db should be considered, and it matches the running container
       expect(provider.exists?).to be true
